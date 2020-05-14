8b995fbbf11d1d10abf35a1add1c75821bdfe7f0
AntonioSE <antonio.salazar@oliverwyman.com>
2020-05-14 16:23:21 -0500

    project complete

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,20 +1,70 @@
-import React, { useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 
 //Context
 import { ModalWindowContext } from '../context/ModalContext';
 
+//Modal
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
 
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
 
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 500,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+
+    },
+}));
 
 const RecipeCard = ({eachRecipe}) => {
 
-    //Local state
-    const [ selectedId, setSelectedId ] = useState(null);
+
+    //Config from material.ui modal
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     //value from context
-    const { setIdDrink } = useContext(ModalWindowContext);
+    const { setIdDrink, drinkIngredients, setDrinkIngredients } = useContext(ModalWindowContext);
 
     const { strDrink, strDrinkThumb, idDrink} = eachRecipe;
+
+    //shows the ingredients
+    const showIngredients = drinkIngredients => {
+        let ingredients = [];
+        for(let i =1; i < 16; i++){
+            if (drinkIngredients[`strIngredient${i}`]) {
+                ingredients.push(
+                <li>{drinkIngredients[`strIngredient${i}`] }
+                    {drinkIngredients[`strMeasure${i}`] }
+                </li>
+                )
+            }
+        }
+        return ingredients
+    }
 
     return (
         <div className="col-sm-6 col-md-4 col-lg-3">
@@ -28,8 +78,32 @@ const RecipeCard = ({eachRecipe}) => {
                         value='View more details'
                         onClick={() => {
                             setIdDrink(idDrink)
+                            handleOpen();
                         }}
                     />
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            setIdDrink(null);
+                            handleClose();
+                            setDrinkIngredients({});
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h2 className='modal__title'>
+                                {drinkIngredients.strDrink}
+                            </h2>
+                            <h3 className="nt-4">Instructions</h3>
+                            <p className='modal__p'>{drinkIngredients.strInstructions}</p>
+                            <figure><img src={drinkIngredients.strDrinkThumb} alt={drinkIngredients.strDrink} className="img-fluid"/></figure>
+                            <h3>Ingredients and portions</h3>
+                                <ul>
+                                    {
+                                        showIngredients(drinkIngredients)
+                                    }
+                                </ul>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -12,11 +12,28 @@ const ModalWindowProvider = (props) => {
 
     //provider state
     const [ idDrink, setIdDrink ] = useState(null);
+    
+    //Local state
+    const [ drinkIngredients, setDrinkIngredients ] = useState({});
+
+    useEffect(() => {
+        const drinkDetails = async () => {
+            if(!idDrink) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+            const detailsCall = await axios(url);
+            setDrinkIngredients(detailsCall.data.drinks[0]);
+        }
+
+        drinkDetails();
+    }, [idDrink]);
+
 
     return (
         <ModalWindowContext.Provider
             value={{
-                setIdDrink
+                setIdDrink,
+                drinkIngredients,
+                setDrinkIngredients
             }}
         >
 
